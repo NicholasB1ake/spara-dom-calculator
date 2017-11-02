123da5503d812f5147717dbbbf0a27a1bf9b950f
nick <nicholasb1ake@hotmail.com>
2017-11-02 01:37:42 +0000

    output sne to console

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -1,4 +1,6 @@
+// Number buttons defined as variables
 var buttons = (document.getElementsByClassName('buttonNum'))
+
 var b7 = (document.getElementsByClassName('buttonNum'))[0];
 var b8 = (document.getElementsByClassName('buttonNum'))[1];
 var b9 = (document.getElementsByClassName('buttonNum'))[2];
@@ -8,3 +10,58 @@ var b6 = (document.getElementsByClassName('buttonNum'))[5];
 var b1 = (document.getElementsByClassName('buttonNum'))[6];
 var b2 = (document.getElementsByClassName('buttonNum'))[7];
 var b3 = (document.getElementsByClassName('buttonNum'))[8];
+// operators defined as variables
+var operator = (document.getElementsByClassName('operator'))
+
+var bmult = (document.getElementsByClassName('operator'))[0];
+var bdiv = (document.getElementsByClassName('operator'))[1];
+var bmin = (document.getElementsByClassName('operator'))[2];
+var badd = (document.getElementsByClassName('operator'))[3];
+// other buttons
+var equals = (document.getElementsByClassName('equals'))
+var clear = (document.getElementsByClassName('buttonClear'))
+
+b7.addEventListener("click", function(){
+  console.log(7)
+
+});
+b8.addEventListener("click", function(){
+  console.log(8)
+
+});
+
+b9.addEventListener("click", function(){
+  console.log(9)
+
+});
+
+b4.addEventListener("click", function(){
+  console.log(4)
+
+});
+
+b5.addEventListener("click", function(){
+  console.log(5)
+
+});
+
+b6.addEventListener("click", function(){
+  console.log(6)
+
+});
+
+b1.addEventListener("click", function(){
+  console.log(1)
+
+});
+
+b2.addEventListener("click", function(){
+  console.log(2)
+
+});
+
+b3.addEventListener("click", function(){
+    console.log(3)
+    
+
+});
