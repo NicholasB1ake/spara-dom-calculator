892e5364f0ebcdb3da725f8d60de31e6811bf408
nick <nicholasb1ake@hotmail.com>
2017-11-02 02:24:41 +0000

    calc takes numbers

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -21,47 +21,86 @@ var badd = (document.getElementsByClassName('operator'))[3];
 var equals = (document.getElementsByClassName('equals'))
 var clear = (document.getElementsByClassName('buttonClear'))
 
+var screen = document.getElementById('screen')
+
 b7.addEventListener("click", function(){
   console.log(7)
+  screen.innerHTML = '7'
 
 });
 b8.addEventListener("click", function(){
   console.log(8)
+  screen.innerHTML = '8'
 
 });
 
 b9.addEventListener("click", function(){
   console.log(9)
-
+    screen.innerHTML = '9'
 });
 
 b4.addEventListener("click", function(){
   console.log(4)
-
+    screen.innerHTML = '4'
 });
 
 b5.addEventListener("click", function(){
   console.log(5)
-
+    screen.innerHTML = '5'
 });
 
 b6.addEventListener("click", function(){
   console.log(6)
-
+    screen.innerHTML = '6'
 });
 
 b1.addEventListener("click", function(){
   console.log(1)
-
+    screen.innerHTML = '1'
 });
 
 b2.addEventListener("click", function(){
   console.log(2)
-
+    screen.innerHTML = '2'
 });
 
 b3.addEventListener("click", function(){
     console.log(3)
-    
+      screen.innerHTML = '3'
 
 });
+
+bmult.addEventListener("click", function(){
+    console.log('*')
+      screen.innerHTML = '*'
+
+});
+
+bdiv.addEventListener("click", function(){
+    console.log('/')
+      screen.innerHTML = '/'
+
+
+});
+
+bmin.addEventListener("click", function(){
+    console.log('-')
+      screen.innerHTML = '-'
+
+});
+
+badd.addEventListener("click", function(){
+    console.log('+')
+      screen.innerHTML = '+'
+
+});
+
+
+
+
+// function add(first, second){
+//   return first + second
+// }
+//
+// var adds = add()
+// console.log(adds)
